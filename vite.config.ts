f30c4051aae08852097c9205a22369b3fdbcb905
Vincent Baures <vincent.baures@360learning.com>
2024-01-17 19:02:11 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "vincentbaures.github.io",
   plugins: [
     vue(),
     vueJsx(),
